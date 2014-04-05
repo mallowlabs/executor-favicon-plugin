4aa37dfea0c513da53301c64379ce94d2264a7bd
mallowlabs <mallowlabs@gmail.com>
2014-04-05 21:13:07 +0900

    Implemented: favicon

---
+++ src/main/java/org/jenkinsci/plugins/executorfavicon/ExecutorFaviconDecorator.java
@@ -0,0 +1,13 @@
+package org.jenkinsci.plugins.executorfavicon;
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+
+@Extension
+public class ExecutorFaviconDecorator extends PageDecorator {
+
+    public ExecutorFaviconDecorator() {
+        super();
+    }
+
+}
